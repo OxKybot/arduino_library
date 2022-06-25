798360c26186c3139dca02f393d9b9864c9f07d7
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-06-25 16:27:40 +0200

    Update OxKybot_JOYSTICK_command.cpp

--- OxKybot_JOYSTICK_command.cpp
+++ OxKybot_JOYSTICK_command.cpp
@@ -12,29 +12,13 @@ void OxKybot_JOYSTICK_command::set_arm(OxKybot_ARM_command _arm)
 }
 void OxKybot_JOYSTICK_command::motor_from_Joymsg(const sensor_msgs::Joy &joy)
 {
-  if (joy.axes[1] == 0 && joy.axes[0] == 0)
-  {
-    motors.motorBrake();
-  }
-  if (joy.axes[1] == 1 && joy.axes[0] == 0)
-  {
-    motors.forward_joy(FAST);
-  }
-
-  if (joy.axes[1] == 0 && joy.axes[0] == 1)
-  {
-    motors.turn_left_joy(FAST);
-  }
-  if (joy.axes[1] == 0 && joy.axes[0] == -1)
-  {
-    motors.turn_right_joy(FAST);
-  }
-  if (joy.axes[1] == -1 && joy.axes[0] == 0)
-  {
-    motors.backward_joy(FAST);
-  }
+  motor_from_joy(FAST);
 }
 void OxKybot_JOYSTICK_command::motor_slow_from_Joymsg(const sensor_msgs::Joy &joy)
+{
+  motor_from_joy(SLOW);
+}
+void OxKybot_JOYSTICK_command::motor_from_joy(motor_speed speed)
 {
   if (joy.axes[1] == 0 && joy.axes[0] == 0)
   {
@@ -42,20 +26,20 @@ void OxKybot_JOYSTICK_command::motor_slow_from_Joymsg(const sensor_msgs::Joy &jo
   }
   if (joy.axes[1] == 1 && joy.axes[0] == 0)
   {
-    motors.forward_joy(SLOW);
+    motors.forward_joy(speed);
   }
 
   if (joy.axes[1] == 0 && joy.axes[0] == 1)
   {
-    motors.turn_left_joy(SLOW);
+    motors.turn_left_joy(speed);
   }
   if (joy.axes[1] == 0 && joy.axes[0] == -1)
   {
-    motors.turn_right_joy(SLOW);
+    motors.turn_right_joy(speed);
   }
   if (joy.axes[1] == -1 && joy.axes[0] == 0)
   {
-    motors.backward_joy(SLOW);
+    motors.backward_joy(speed);
   }
 }
 void OxKybot_JOYSTICK_command::angle_from_Joymsg(const sensor_msgs::Joy &joy)
