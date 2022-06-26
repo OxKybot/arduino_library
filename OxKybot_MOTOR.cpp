ed0c858b3e6993b222208e1dde8dcb641d7b373d
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-06-26 16:26:57 +0200

    Update OxKybot_MOTOR.cpp

--- OxKybot_MOTOR.cpp
+++ OxKybot_MOTOR.cpp
@@ -17,6 +17,7 @@ void OxKybot_MOTOR::setLogger(Logger l)
 }
 void OxKybot_MOTOR::loop()
 {
+  this->logger.publish_arduino_log("transition = "+String(this->transitionType));
   if(this->transitionType == NO_TRANSITION && this->actualSpeed == SLOW)
   {
     if(this->actualState == FORWARD)
