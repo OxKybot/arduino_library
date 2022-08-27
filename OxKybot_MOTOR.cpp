bd4d729ccdd0d4185a577c41cdd8ab7344c7ac32
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-08-27 18:46:43 +0200

    Update OxKybot_MOTOR.cpp

--- OxKybot_MOTOR.cpp
+++ OxKybot_MOTOR.cpp
@@ -33,8 +33,8 @@ void OxKybot_MOTOR::loop()
       case FORWARD:
         motor_Forward(this->actualSpeed);
         break;
-        case FORWARD:
-        motor_Forward(this->actualSpeed);
+       case BCKWARD:
+        motor_Backward(this->actualSpeed);
         break;
     }
   }
