96245df9dcb33db0579dcf306822f3983b3ceb17
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-09-03 17:49:46 +0200

    Update OxKybot_JOYSTICK_command.h

--- OxKybot_JOYSTICK_command.h
+++ OxKybot_JOYSTICK_command.h
@@ -12,13 +12,14 @@ public:
     void set_motors(OxKybot_MOTOR_command _motors);
     void set_arm(OxKybot_ARM_command _arm);
     void motor_from_Joymsg(const sensor_msgs::Joy &joy);
-    void motor_slow_from_Joymsg(const sensor_msgs::Joy &joy);
     void axe_from_Joymsg(const sensor_msgs::Joy &joy, bool isLeft);
     void angle_from_Joymsg(const sensor_msgs::Joy &joy);
 
 private:
     OxKybot_MOTOR_command motors;
     OxKybot_ARM_command arm;
+    void gestionCroixDirectionnel(int hori, int verti);
+    boolean isDirectionStarted;
     void motor_from_joy(motor_speed speed,const sensor_msgs::Joy &joy);
 };
 #endif
