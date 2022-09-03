afdfb10d524cf75c8731baccffe5c59e8f463c9a
OxKybot <103052308+OxKybot@users.noreply.github.com>
2022-09-03 17:47:46 +0200

    Update OxKybot_JOYSTICK_command.cpp

--- OxKybot_JOYSTICK_command.cpp
+++ OxKybot_JOYSTICK_command.cpp
@@ -1,7 +1,9 @@
 #include "OxKybot_JOYSTICK_command.h"
 
 OxKybot_JOYSTICK_command::OxKybot_JOYSTICK_command()
-{}
+{
+    this->isDirectionStarted = false;
+}
 void OxKybot_JOYSTICK_command::set_motors(OxKybot_MOTOR_command _motors)
 {
     motors = _motors;
@@ -10,37 +12,44 @@ void OxKybot_JOYSTICK_command::set_arm(OxKybot_ARM_command _arm)
 {
     arm = _arm;
 }
-void OxKybot_JOYSTICK_command::motor_from_Joymsg(const sensor_msgs::Joy &joy)
+void OxKybot_JOYSTICK_command::motor_from_joy(const sensor_msgs::Joy &joy)
 {
-  motor_from_joy(FAST,joy);
+    gestionCroixDirectionnel(joy.axes[7],joy.axes[8]);
+    motors.motor_left_joy(joy.axes[2]*100);
+    motors.motor_right_joy(joy.axes[6]*100);
 }
-void OxKybot_JOYSTICK_command::motor_slow_from_Joymsg(const sensor_msgs::Joy &joy)
-{
-  motor_from_joy(SLOW,joy);
-}
-void OxKybot_JOYSTICK_command::motor_from_joy(motor_speed speed, const sensor_msgs::Joy &joy)
-{
-  if (joy.axes[1] == 0 && joy.axes[0] == 0)
-  {
-    motors.motorBrake();
-  }
-  if (joy.axes[1] == 1 && joy.axes[0] == 0)
-  {
-    motors.forward_joy(speed);
-  }
 
-  if (joy.axes[1] == 0 && joy.axes[0] == 1)
-  {
-    motors.turn_left_joy(speed);
-  }
-  if (joy.axes[1] == 0 && joy.axes[0] == -1)
-  {
-    motors.turn_right_joy(speed);
-  }
-  if (joy.axes[1] == -1 && joy.axes[0] == 0)
-  {
-    motors.backward_joy(speed);
-  }
+void OxKybot_JOYSTICK_command::gestionCroixDirectionnel(int hori, int verti)
+{
+    if(verti != 0 || hori != 0)
+    {
+        this->isDirectionStarted = true;
+    }
+    if(this->isDirectionStarted)
+    {
+        if (verti == 0 && hori == 0)
+        {
+            motors.motorBrake();
+            this->isDirectionStarted = false;
+        }
+        if (verti == 1 && hori == 0)
+        {
+            motors.forward_joy();
+        }
+
+        if (verti == 0 && hori == 1)
+        {
+            motors.turn_left_joy();
+        }
+        if (verti == 0 && hori == -1)
+        {
+            motors.turn_right_joy();
+        }
+        if (verti == -1 && hori == 0)
+        {
+            motors.backward_joy();
+        }
+    }
 }
 void OxKybot_JOYSTICK_command::angle_from_Joymsg(const sensor_msgs::Joy &joy)
 {
